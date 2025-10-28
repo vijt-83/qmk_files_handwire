72a2bb874ed1d60457e6ecbbc00a151c120741b0
_iny.tdo_ <154915118+vijt-83@users.noreply.github.com>
2025-10-28 23:22:50 +0700

    Add files via upload

---
+++ ergo_s/config.h
@@ -0,0 +1,11 @@
+#define master_left
+
+#define ENCODER_MAP_KEY_DELAY 5
+
+#define ENCODER_A_PINS { GP13 }
+#define ENCODER_B_PINS { GP15 }
+#define ENCODER_RESOLUTIONS { 2 }
+#define ENCODER_A_PINS_RIGHT { GP15 }
+#define ENCODER_B_PINS_RIGHT { GP13 }
+#define ENCODER_RESOLUTIONS_RIGHT { 2 }
+
---
+++ ergo_s/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+// Copyright 2022 Victor Lucachi (@victorlucachi)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCC, 0xD5, 0xC0, 0x1C, 0x64, 0xB6, 0x49, 0x5F}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
