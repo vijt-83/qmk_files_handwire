1ed6a010f081298a6473254ad391cb1ea433f767
_iny.tdo_ <154915118+vijt-83@users.noreply.github.com>
2025-10-28 23:26:18 +0700

    Add files via upload
    Signed-off-by: _iny.tdo_ <154915118+vijt-83@users.noreply.github.com>

---
+++ split_test_v2/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x41, 0x15, 0xA0, 0xF2, 0x04, 0x79, 0xA2, 0x82}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 2 }
---
+++ wakizashi_40/keymaps/vial/config.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x77, 0x6D, 0x1A, 0x79, 0x12, 0x62, 0x91, 0x73}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
